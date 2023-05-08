2ed9536ab5e09ee7b329da42d73640744dae2c1e
Canoral <kaya68@outlook.fr>
2023-05-08 12:44:01 +0200

    feat: Mise en place de L'API

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Posts from "../Posts/Posts";
 
+import axios from "axios";
+
 import "./App.scss";
 
-import categoriesData from "../../data/categories";
 import postsData from "../../data/posts";
+import { ICategories, IPost } from "../../@interfaces/interfaces";
 
 function App() {
   const [zenMode, setZenMode] = useState(false);
+  const [categories, setCategories] = useState<ICategories[]>([]);
+  const [posts, setPosts] = useState<IPost[]>([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const categoriesData = await axios.get(
+          "https://oclock-open-apis.vercel.app/api/blog/categories"
+        );
+        const postsData = await axios.get(
+          "https://oclock-open-apis.vercel.app/api/blog/posts"
+        );
+        setCategories(categoriesData.data);
+        setPosts(postsData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [categories, posts]);
   return (
     <div className={`App ${zenMode ? "zen-mode" : ""}`}>
       <Header
-        categories={categoriesData}
+        categories={categories}
         zenMode={zenMode}
         setZenMode={setZenMode}
       />
-      <Posts posts={postsData} />
+      <Posts posts={posts} />
       <Footer />
     </div>
   );
