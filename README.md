696b4e5e4086eed97507d4486e7ebae44fdcf6f4
Canoral <kaya68@outlook.fr>
2023-05-08 13:49:52 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Blog-exemple
+
+Un exemple de blog qui a été crée à l'aide de React et Typescript
+
+## Description
+
+Ce projet est un exemple de blog développé en utilisant React. Il récupère les données des catégories et des articles à partir de l'API publique https://oclock-open-apis.vercel.app/api/blog et les affiche sur la page. Le blog comprend un en-tête avec une barre de menu pour les catégories et un bouton pour activer le mode zen, ainsi qu'un pied de page avec des informations sur le blog. Le projet utilise également des interfaces TypeScript pour garantir le typage des données.
+
+## Technologies utilisées
+
+React, Axios, TypeScript, SASS.
+
+## Fonctionnalités
+
+- Affichage des catégories et des articles à partir de l'API publique.
+- Possibilité d'activer le mode zen pour une expérience utilisateur plus agréable.
+- Utilisation d'interfaces TypeScript pour garantir le typage des données.
+- Design responsive et moderne avec SASS.
+
+## Demonstration
+
+![giphy](./public/giphy.gif)
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Posts from "../Posts/Posts";
+import Spinner from "../Spinner/Spinner";
 
 import axios from "axios";
 
 import "./App.scss";
 
-import postsData from "../../data/posts";
 import { ICategories, IPost } from "../../@interfaces/interfaces";
 
 function App() {
   const [zenMode, setZenMode] = useState(false);
   const [categories, setCategories] = useState<ICategories[]>([]);
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -26,12 +27,14 @@ function App() {
         );
         setCategories(categoriesData.data);
         setPosts(postsData.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, [categories, posts]);
+  }, []);
+
   return (
     <div className={`App ${zenMode ? "zen-mode" : ""}`}>
       <Header
@@ -39,7 +42,8 @@ function App() {
         zenMode={zenMode}
         setZenMode={setZenMode}
       />
-      <Posts posts={posts} />
+      {loading ? <Spinner /> : <Posts posts={posts} />}
+
       <Footer />
     </div>
   );
