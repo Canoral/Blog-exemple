92a51d9382c40a275c26abcf616eb70a85447aca
Canoral <kaya68@outlook.fr>
2023-05-09 10:23:28 +0200

    feat: Gérer les erreurs

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,12 +1,33 @@
-import './NotFound.scss';
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+import "./NotFound.scss";
 
 function NotFound() {
+  const error = useRouteError();
+  console.log(error);
+
+  function getErrorMessage(e: unknown): string {
+    if (isRouteErrorResponse(e)) {
+      return e.statusText;
+    }
+
+    if (e instanceof Error) {
+      return e.message;
+    }
+
+    if (typeof e === "string") {
+      return e;
+    }
+
+    return "Unknown error";
+  }
+
   return (
     <div className="not-found">
       <h1>404</h1>
       <p>Désolé, une erreur inattendue est survenue.</p>
       <p>
-        <i>Not Found</i>
+        <i>{getErrorMessage(error)}</i>
       </p>
     </div>
   );
